cf5c92591fa28921784fbc6ffdeced6db0295636
Štěpán Tomsa <stomsa@redhat.com>
2019-12-05 21:43:22 +0100

    More working.

--- 2019/Day 2/lib/solver.rb
+++ 2019/Day 2/lib/solver.rb
@@ -1,40 +1,39 @@
 module Solver
+  Instruction = Struct.new(:num_args, :function)
+  
   INSTRUCTIONS = {
-    1 => lambda do |input, cur_i|
-      with_args(input, cur_i, 3) do |output, x1_i, x2_i, target_i|
-        output[target_i] = input[x1_i] + input[x2_i]
-      end
-    end,
-    2 => lambda do |input, cur_i|
-      with_args(input, cur_i, 3) do |output, x1_i, x2_i, target_i|
-        output[target_i] = input[x1_i] * input[x2_i]
-      end
-    end,
-    99 => lambda do |input, cur_i|
-      return input, input.length
-    end
+    1 => Instruction.new(3, lambda do |input, x1_i, x2_i, target_i|
+      output = input.dup
+      output[target_i] = input[x1_i] + input[x2_i]
+      output
+    end),
+    2 => Instruction.new(3, lambda do |input, x1_i, x2_i, target_i|
+      output = input.dup
+      output[target_i] = input[x1_i] * input[x2_i]
+      output
+    end),
+    99 => Instruction.new(0, lambda do |input|
+      return nil
+    end)
   }
   
   def execute(intcode)
     i = 0
     while i < intcode.length do
-      instruction = intcode[i]
-      raise RuntimeError unless INSTRUCTIONS.has_key?(instruction)
-      intcode, i = INSTRUCTIONS[instruction].call(intcode, i)
+      instruction_code = intcode[i]
+      raise RuntimeError unless INSTRUCTIONS.has_key?(instruction_code)
+      
+      instruction = INSTRUCTIONS[instruction_code]
+      
+      i = i.succ
+      args = intcode[i...i + instruction.num_args]
+      result = instruction.function.call(intcode, *args)
+      result ? intcode = result : break
+      
+      i += instruction.num_args
     end
     intcode
   end
   
-  private
-  def with_args(input, index, count)
-    output = input.dup
-    
-    first = index + 1
-    last = index + count
-    yield output, *input[first..last]
-    
-    return output, first + count
-  end
-  
-  module_function(:execute, :with_args)
+  module_function(:execute)
 end

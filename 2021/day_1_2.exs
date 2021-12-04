a4718d48cc87e08c8f2a6e355e3b40fb1707da1a
Glutexo <glutexo@icloud.com>
2021-12-04 21:19:17 +0100

    Day 1 Task 2 progress

--- 2021/day_1_2.exs
+++ 2021/day_1_2.exs
@@ -1,11 +1,11 @@
 defmodule Day1 do
-  defmodule Windows do
-    defstruct previous: [], current: []
-  end
+#  defmodule Windows do
+#    defstruct previous: [], current: []
+#  end
   
-  defmodule Accumulator do
-    defstruct count: 0, windows: %Windows{}
-  end
+#  defmodule Accumulator do
+#    defstruct count: 0, windows: %Windows{}
+#  end
   
   defmodule Window do
     def shift(windows) do
@@ -43,19 +43,19 @@ defmodule Day1 do
     input
     |> Enum.map(&String.trim_trailing/1)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce(%Accumulator{}, &reduce/2)
+#    |> Enum.reduce(%Accumulator{}, &reduce/2)
     |> Map.get(:count)
     |> Integer.to_string()
   end
 
-  defp reduce(_elem, _accumulator = %Accumulator{}) do
-    
-  end
+#  defp reduce(_elem, _accumulator = %Accumulator{}) do
+#    
+#  end
 
   def next(elem, old_previous, old_current) do
-    new_previous = old_previous[1..2] ++ [old_current[0]]
-    new_current = old_current[1..2] ++ [elem]
-    %Windows{previous: new_previous, current: new_current}
+    _new_previous = old_previous[1..2] ++ [old_current[0]]
+    _new_current = old_current[1..2] ++ [elem]
+#    %Windows{previous: new_previous, current: new_current}
   end
   
   def append(list, elem) do
@@ -65,14 +65,128 @@ defmodule Day1 do
 end
 
 
-Day1.Window.shift(
-  [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [10]
-  ]
-) |> IO.inspect(charlists: :as_lists)
+defmodule Windows do
+  defmodule Accumulator do
+    defstruct(result: [], number: nil)
+  end
+
+  def run(input, number) do
+    Enum.reduce(
+      Enum.reverse(input),
+      %Accumulator{number: number},
+      &reduce/2
+    )
+  end
+  
+  defp reduce(element, accumulator) do
+    window = push(element, accumulator.number)
+    result = [window | accumulator.result]
+    number = Enum.at(element, 0)
+    %Accumulator{result: result, number: number}
+  end
+
+  defp push(window, number) do
+    Enum.slice(window, 1..-1) ++ [number]
+  end
+end
+
+defmodule WindowsTest do
+  defmodule TestData do
+    defstruct(input: [], number: nil, result: [])
+  end
+
+  defmodule TestResult do
+    defstruct([:input, :number, :result])
+  end
+
+  defmodule Tests do
+    @test_data [
+      %TestData{
+	input: [[0, 1, 2]],
+	number: 3,
+	result: %Windows.Accumulator{
+	  result: [[1, 2, 3]],
+	  number: 0
+	},
+      },
+      %TestData{
+	input: [[0, 1, 2], [3, 4, 5]],
+	number: 6,
+	result: %Windows.Accumulator{
+	  result: [[1, 2, 3], [4, 5, 6]],
+	  number: 0,
+	},
+      },
+      %TestData{
+	input: [[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+	number: 9,
+	result: %Windows.Accumulator{
+	  result: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+	  number: 0,
+	},
+      },
+      %TestData{
+	input: [[0, 1]],
+	number: 2,
+	result: %Windows.Accumulator{
+	  result: [[1, 2]],
+	  number: 0,
+	},
+      },
+      %TestData{
+	input: [[0, 1], [2, 3]],
+	number: 4,
+	result: %Windows.Accumulator{
+	  result: [[1, 2], [3, 4]],
+	  number: 0,
+	},
+      },
+      %TestData{
+	input: [[0, 1], [2, 3], [4, 5]],
+	number: 6,
+	result: %Windows.Accumulator{
+	  result: [[1, 2], [3, 4], [5, 6]],
+	  number: 0,
+	},
+      },
+      %TestData{
+	input: [[0]],
+	number: 1,
+	result: %Windows.Accumulator{
+	  result: [[1]],
+	  number: 0,
+	},
+      },
+    ]
+
+    def run() do
+      Enum.map(@test_data, &test/1)
+    end
+
+    defp test(%TestData{input: input, number: number, result: result}) do
+      test_result = Windows.run(input, number) == result
+      %TestResult{input: input, number: number, result: test_result}
+    end    
+  end
+  
+end
+
+Windows.run([[0, 1, 2]], 3)
+|> IO.inspect()
+WindowsTest.Tests.run()
+|> IO.inspect()
+
+
+#input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+#Day1.Window.shift(
+#  [
+#    [1, 2, 3],
+#    [4, 5, 6],
+#    [7, 8, 9],
+#    [10]
+#  ]
+#) |> IO.inspect(charlists: :as_lists)
 
 #input = File.stream!("input2.txt")
 #output = Day1.solve(input) <> "\n"

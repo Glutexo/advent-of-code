998a7462dabb0c61846b887af1b565d80624bd8a
Glutexo <glutexo@icloud.com>
2021-12-14 22:27:58 +0100

    Remove unnecessary modules

--- 2021/day_1_2.exs
+++ 2021/day_1_2.exs
@@ -1,61 +1,33 @@
 defmodule Day1 do
   def solve(input) do
     input
-    |> Groups.create(3)
-    |> Enum.map(&Enum.sum/1)
-    |> Groups.create(2)
-    |> Enum.count(fn [a, b] -> b > a end)
-  end
-end
-
-
-defmodule InputOutput do
-  def input(stream) do
-    stream
     |> Enum.map(&String.trim_trailing/1)
     |> Enum.map(&String.to_integer/1)
+    |> group(3)
+    |> Enum.map(&Enum.sum/1)
+    |> group(2)
+    |> Enum.count(fn [a, b] -> b > a end)
+    |> Integer.to_string()
   end
 
-  def output(integer) do
-    Integer.to_string(integer) <> "\n"
-  end
-end
-
-
-defmodule Groups do
-  def create(values, size) do
-    values
-    |> initialize()
-    |> filter(size)
-    |> slice(size)
-  end
-
-  def initialize(values) do
-    Enum.reduce(values, [[]], &reduce/2)
+  defp group(values, size) do
+    Enum.reduce(
+      values,
+      [[]],
+      fn element, accumulator ->
+        [[] | Enum.map(accumulator, &([element | &1]))]
+      end
+    )
     |> Enum.map(&Enum.reverse/1)
     |> Enum.reverse()
+    |> Enum.filter(&(length(&1) >= size))
+    |> Enum.map(&(Enum.slice(&1, 0, size)))
   end
-
-  defp reduce(element, accumulator) do
-    [[] | Enum.map(accumulator, &([element | &1]))]
-  end
-
-  defp filter(windows, size) do
-    Enum.filter(windows, &(length(&1) >= size))
-  end
-
-  defp slice(windows, size) do
-    Enum.map(windows, &(Enum.slice(&1, 0, size)))
-  end
-
 end
 
-
-
-# output = File.stream!("input1.txt")
-output = File.stream!("input2.txt")
-|> InputOutput.input()
+# result = File.stream!("input1.txt")
+result = File.stream!("input2.txt")
 |> Day1.solve()
-|> InputOutput.output()
+output = result <> "\n"
 IO.write(output)
 File.write!("output2.txt", output)
